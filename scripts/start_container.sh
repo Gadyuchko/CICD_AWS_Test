adce2d69137ddb37c60305371ee96b1c492eee5f
Roman <gadyuchko.roman@gmail.com>
2023-05-09 11:38:05 +0300

    Deleting dev branch

---
+++ scripts/start_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Pull and start docker images"
+export AWS_ACCOUNT_ID=AWS_ACCOUNT_ID_ENV
+export AWS_DEFAULT_REGION=AWS_DEFAULT_REGION_ENV
+export AWS_IMAGE_REPO_NAME=AWS_IMAGE_REPO_NAME_ENV
+export AWS_IMAGE_TAG=AWS_IMAGE_TAG_ENV
+cd ~
+docker compose pull
+docker compose up -d
