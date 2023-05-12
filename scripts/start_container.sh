8e8e9451c9279018964cfbb14ce3a1e75c62b990
Roman <gadyuchko.roman@gmail.com>
2023-05-12 15:26:49 +0300

    Added deploy stage to pipeline, added infrastructure template, provisioned resources, altered scripts for container added app spec. Untested.

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,5 +5,5 @@ export AWS_DEFAULT_REGION=AWS_DEFAULT_REGION_ENV
 export AWS_IMAGE_REPO_NAME=AWS_IMAGE_REPO_NAME_ENV
 export AWS_IMAGE_TAG=AWS_IMAGE_TAG_ENV
 cd ~
-docker compose pull
-docker compose up -d
+docker-compose pull
+docker-compose up -d
