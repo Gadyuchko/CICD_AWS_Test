3583eb29ad6de7cdac929a99071b8b98aedf0610
Roman <gadyuchko.roman@gmail.com>
2023-05-15 12:34:28 +0300

    Testing pipeline: alteration 10 changes to infrastructure and codedeploy scripts

--- scripts/kill_container.sh
+++ scripts/kill_container.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Stopping and removing the running container"
 cd ~
+export AWS_ACCOUNT_ID=AWS_ACCOUNT_ID_ENV
+export AWS_DEFAULT_REGION=AWS_DEFAULT_REGION_ENV
+export AWS_IMAGE_REPO_NAME=AWS_IMAGE_REPO_NAME_ENV
+export AWS_IMAGE_TAG=AWS_IMAGE_TAG_ENV
 docker-compose stop
 docker-compose rm -f
